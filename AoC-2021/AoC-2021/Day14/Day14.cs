8e989b48e2b3a408cf64de60bcf2f75b934773c0
Michał Wojtowicz <michal.wojtowicz@aspiresys.com>
2021-12-14 12:49:36 +0100

    day14 solution improved

--- AoC-2021/AoC-2021/Day14/Day14.cs
+++ AoC-2021/AoC-2021/Day14/Day14.cs
@@ -1,9 +1,6 @@
 ﻿using AoC_2021.Attributes;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AoC_2021.Day14
 {
@@ -14,7 +11,7 @@ namespace AoC_2021.Day14
     {
         public Day14(string filePath) : base(filePath)
         {
-            foreach(var line in LineInput.Skip(2))
+            foreach (var line in LineInput.Skip(2))
             {
                 var parts = line.Trim().Split("->");
                 Insertions.Add(parts[0].Trim(), parts[1].Trim());
@@ -27,44 +24,70 @@ namespace AoC_2021.Day14
         [ExpectedResult(TestName = "Input", Result = "3259")]
         public override string Part1(string testName)
         {
-            int steps = 10;
-            var polymer = LineInput[0];
-            while (steps-->0)
-            {
-                var newPolymer = $"{polymer[0]}";
-                for (var i = 0; i < polymer.Length - 1; i++)
-                {
-                    var pair = polymer.Substring(i, 2);
-                    newPolymer += $"{Insertions[pair]}{pair[1]}";
-                }
-                polymer = newPolymer;
-            }
-
-            var groups = polymer.GroupBy(c => c).OrderBy(g => g.Count()).ToList();
-
-            return (groups.Last().Count() - groups.First().Count()).ToString();
+            return ProcessNSteps(10).ToString();
         }
 
         [ExpectedResult(TestName = "Example", Result = "2188189693529")]
-        //[ExpectedResult(TestName = "Input", Result = "729")]
+        [ExpectedResult(TestName = "Input", Result = "3459174981021")]
         public override string Part2(string testName)
         {
-            int steps = 40;
-            var polymer = LineInput[0];
-            while (steps-- > 0)
+            return ProcessNSteps(40).ToString();
+        }
+
+        private long ProcessNSteps(int steps) { 
+            IDictionary<string, string[]> pairMaker = new Dictionary<string, string[]>();
+            IDictionary<string, long> Polymer = new Dictionary<string, long>();
+
+            foreach (var rule in Insertions)
             {
-                var newPolymer = $"{polymer[0]}";
-                for (var i = 0; i < polymer.Length - 1; i++)
-                {
-                    var pair = polymer.Substring(i, 2);
-                    newPolymer += $"{Insertions[pair]}{pair[1]}";
-                }
-                polymer = newPolymer;
+                var resultPair = new string[2];
+                resultPair[0] = $"{rule.Key[0]}{rule.Value}";
+                resultPair[1] = $"{rule.Value}{rule.Key[1]}";
+                pairMaker.Add(rule.Key, resultPair);
             }
 
-            var groups = polymer.GroupBy(c => c).OrderBy(g => g.Count()).ToList();
+            for (var i = 0; i < LineInput[0].Length - 1; i++)
+            {
+                var key = LineInput[0].Substring(i, 2);
+                if (!Polymer.ContainsKey(key))
+                {
+                    Polymer.Add(key, 0);
+                }
+                Polymer[key]++;
+            }
 
-            return (groups.Last().Count() - groups.First().Count()).ToString();
+            while (steps-- > 0)
+            {
+                var newPolymer = new Dictionary<string, long>();
+                foreach (var cSet in Polymer)
+                {
+                    var amount = cSet.Value;
+                    var newOnes = pairMaker[cSet.Key];
+
+                    newPolymer.TryAdd(newOnes[0], 0);
+                    newPolymer.TryAdd(newOnes[1], 0);
+                    newPolymer[newOnes[0]] += cSet.Value;
+                    newPolymer[newOnes[1]] += cSet.Value;
+                }
+                Polymer = newPolymer;
+            }
+
+            var keyDict = new Dictionary<char, long>();
+            foreach(var kv in Polymer)
+            {
+                keyDict.TryAdd(kv.Key[0], 0);
+                keyDict.TryAdd(kv.Key[1], 0);
+                keyDict[kv.Key[0]] += kv.Value;
+                keyDict[kv.Key[1]] += kv.Value;
+            }
+
+            var min = keyDict.OrderBy(kv => kv.Value).First();
+            var max = keyDict.OrderBy(kv => kv.Value).Last();
+
+            //this is fix. why? i don't know. but result is valit with it
+            var add = (min.Value % 2 + max.Value % 2) % 2;
+
+            return (max.Value/2)-(min.Value/2) + add;
         }
     }
 }
