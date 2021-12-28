9ef441066f8815f22122ebe71d0022a54464b8d7
misiakw <misiakw@gmail.com>
2021-12-28 21:19:37 +0100

    day 22

--- AoC-2021/AoC-2021/Day22/Day22.cs
+++ AoC-2021/AoC-2021/Day22/Day22.cs
@@ -6,112 +6,122 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AoC_2021.Day22.Cuboid;
+using Range = AoC_2021.Day22.Cuboid.Range;
 
 namespace AoC_2021.Day22
 {
     [BasePath("Day22")]
     [TestFile(File = "example.txt", Name = "Example")]
     [TestFile(File = "Input.txt", Name = "Input")]
-    public class Day22 : ParseableDay<Cuboid>
+    public class Day22 : DayBase
     {
-        public Day22(string path) : base(path){ }
+        public Day22(string path) : base(path) { }
 
-        public override Cuboid Parse(string input)
+        public Tuple<bool, Cuboid> Parse(string input) => Parse(input, long.MinValue, long.MaxValue);
+
+        public Tuple<bool, Cuboid> Parse(string input, long minLimit, long maxLimit)
         {
-            return new Cuboid(input);
+            var parts = input.Trim().Split(" ");
+            var state = parts[0].Equals("on");
+            var ranges = parts[1].Split(',').Select(l => l.Trim().Substring(2).Split("..").Select(s => long.Parse(s)).ToArray()).ToArray();
+
+            var x = new Range(ranges[0][0], ranges[0][1], Axis.X);
+            var y = new Range(ranges[1][0], ranges[1][1], Axis.Y);
+            var z = new Range(ranges[2][0], ranges[2][1], Axis.Z);
+            return Tuple.Create(state, new Cuboid(x, y, z));
         }
 
         [ExpectedResult(TestName = "Example", Result = "474140")]
         [ExpectedResult(TestName = "Input", Result = "601104")]
         public override string Part1(string testName)
         {
-            var turnedOn = new List<Cuboid>();
-            var minLimit = -50;
-            var maxLimit = 50;
-            var steps = Input.Where(i =>
-            {
-                if (i.MaxX < minLimit || i.MinX > maxLimit) return false;
-                if (i.MaxY < minLimit || i.MinY > maxLimit) return false;
-                if (i.MaxZ < minLimit || i.MinZ > maxLimit) return false;
-                return true;
-            }).ToList();
+            var turnedList = new List<Cuboid>();
+            var Input = LineInput.Select(l => Parse(l, -50, 50)).ToList();
 
-            foreach(var cuboid in steps)
+            foreach (var opeation in Input)
             {
-                var toIntersect = turnedOn.Where(c => c.Intersect(cuboid)).ToList();
-                foreach (var intersect in toIntersect)
+                var newSet = new List<Cuboid>();
+                foreach (var turnedOn in turnedList)
                 {
-                    turnedOn.Remove(intersect);
-                    turnedOn.AddRange(intersect.Slice(cuboid));
+                    if (opeation.Item1)
+                        newSet.AddRange(turnedOn.Add(opeation.Item2));
+                    else
+                        newSet.AddRange(turnedOn.Substract(opeation.Item2));
                 }
-                if (!toIntersect.Any() && cuboid.State)
-                    turnedOn.Add(cuboid);
+                turnedList = newSet;
             }
 
-            return turnedOn.Select(c => c.Volume).Sum().ToString();
+            return turnedList.Select(c => c.Volume).Sum().ToString();
         }
 
         [ExpectedResult(TestName = "Example", Result = "2758514936282235")]
         //[ExpectedResult(TestName = "Input", Result = 571032)]
         public override string Part2(string testName)
         {
-            var turnedOn = new List<Cuboid>();
+            var turnedList = new List<Cuboid>();
+            var Input = LineInput.Select(l => Parse(l)).ToList();
 
-            foreach (var cuboid in Input)
+            foreach (var opeation in Input)
             {
-                var toIntersect = turnedOn.Where(c => c.Intersect(cuboid)).ToList();
-                foreach (var intersect in toIntersect)
+                var newSet = new List<Cuboid>();
+                foreach (var turnedOn in turnedList)
                 {
-                    turnedOn.Remove(intersect);
-                    turnedOn.AddRange(intersect.Slice(cuboid));
+                    if (opeation.Item1)
+                        newSet.AddRange(turnedOn.Add(opeation.Item2));
+                    else
+                        newSet.AddRange(turnedOn.Substract(opeation.Item2));
                 }
-                if (!toIntersect.Any() && cuboid.State)
-                    turnedOn.Add(cuboid);
+                turnedList = newSet;
             }
 
-            return turnedOn.Select(c => c.Volume).Sum().ToString();
+            return turnedList.Select(c => c.Volume).Sum().ToString();
         }
     }
     public class Cuboid
     {
-        public readonly bool State;
-        public readonly long MinX, MaxX, MinY, MaxY, MinZ, MaxZ;
-        public long Volume => (MaxX - MinX) * (MaxY - MinY) * (MaxZ - MinZ);
-        public Cuboid(long minX, long maxX, long minY, long maxY, long minZ, long maxZ)
+        public readonly IReadOnlyDictionary<Axis, Range> Ranges;
+        public long Volume => Ranges.Values.Select(v => v.Span).Aggregate(1L, (x, y) => x * y);
+        public Cuboid(Range X, Range Y, Range Z)
         {
-            MinX = minX;
-            MaxX = maxX;
-            MinY = minY;
-            MaxY = maxY;
-            MinZ = minZ;
-            MaxZ = maxZ;
-        }
-        public Cuboid(string input)
-        {
-            var parts = input.Trim().Split(" ");
-            State = parts[0].Equals("on");
-            var ranges = parts[1].Split(',').Select(l => l.Trim().Substring(2).Split("..").Select(s => long.Parse(s)).ToArray()).ToArray();
-
-            MinX = ranges[0][0];
-            MaxX = ranges[0][1];
-            MinY = ranges[1][0];
-            MaxY = ranges[1][1];
-            MinZ = ranges[2][0];
-            MaxZ = ranges[2][1];
+            Ranges = new Dictionary<Axis, Range>() {
+                {X.Axis, X}, {Y.Axis, Y}, {Z.Axis, Z}
+            };
         }
 
-        public bool Intersect(Cuboid other)
+        public IEnumerable<Cuboid> Add(Cuboid other)
         {
-            //ToDo: implement logic to verify if cuboids ocupy the same space
-            return false;
-        }
-
-        public IEnumerable<Cuboid> Slice(Cuboid other)
-        {
-            //ToDo: Implement slicing cuboids. return all shigning parts;
             yield return this;
-            if (other.State)
-                yield return other;
+            yield return other;
+        }
+
+        public IEnumerable<Cuboid> Substract(Cuboid other)
+        {
+            yield return this;
+        }
+
+        public Range GetRange(Axis axis) => Ranges[axis];
+
+        /*public Cuboid Slice(Range range)
+        {
+            var rangeX = range.Axis == Axis.X ? range : GetRange(Axis.X);
+            var rangeY = range.Axis == Axis.Y ? range : GetRange(Axis.Y);
+            var rangeZ = range.Axis == Axis.Y ? range : GetRange(Axis.Z);
+            return new Cuboid(rangeX, rangeY, rangeZ);
+        }*/
+
+        public enum Axis { X, Y, Z }
+        public class Range
+        {
+            public readonly long Min, Max;
+            public readonly Axis Axis;
+            public Range(long min, long max, Axis axis)
+            {
+                Min = min;
+                Max = max;
+                Axis = axis;
+            }
+            public long Span => Max - Min;
         }
     }
 }
