15092dadfd4acf124c80a6c206adb0943694d808
Michał Wojtowicz <michal.wojtowicz@aspiresys.com>
2022-11-21 17:35:57 +0100

    Day1 Completed

--- AoC2016/Day1.cs
+++ AoC2016/Day1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AoC2016.Common;
 using Step = AoC2016.Days.Day1.Step;
 
 namespace AoC2016
@@ -12,12 +13,17 @@ namespace AoC2016
         public Day1() : base(1, false)
         {
             Input("example1")
-                .RunPart(1, 42)
-                .RunPart(2)
+                .RunPart(1, 5)
             .Input("example2")
-                .RunPart(1)
+                .RunPart(1, 2)
             .Input("example3")
-            .Input("output");
+                .RunPart(1, 12)
+            .Input("example4")
+                .Silent(1)
+                .RunPart(2, 4)
+            .Input("output")
+                .RunPart(1, 226)
+                .RunPart(2, 79);
         }
 
         public override Step Parse(string val) => new Step{
@@ -28,15 +34,68 @@ namespace AoC2016
         public override IList<string> Split(string val) => val.Split(",");
         public override object Part1(IList<Step> data, Input input)
         {
-            return 15;
+            var compass = new Compass();
+            foreach(var step in data)
+                compass.processStep(step);
+
+            input.Cache = compass;
+            return compass.Dist;
         }
 
 
         public override object Part2(IList<Step> data, Input input)
         {
-            return "Part 2";
+            var compass = (Compass) input.Cache;
+
+            return compass?.CrossDist?.FirstOrDefault();
         }
 
+        private class Compass{
+            private int X, Y;
+            private char dir = 'N';
+            private Array2D<bool> map = new Array2D<bool>();
+            public IList<int> CrossDist = new List<int>();
+
+            public int Dist {
+                get =>  Math.Abs(X) + Math.Abs(Y);
+            }
+
+            public void processStep(Step step){
+                Rotate(step.Rot);
+
+                switch(dir){
+                    case 'N': processStep(1, 0, step.Len); break;
+                    case 'S': processStep(-1, 0, step.Len); break;
+                    case 'E': processStep(0, 1, step.Len); break;
+                    case 'W': processStep(0, -1, step.Len); break;
+                }                
+            }
+            private void processStep(int dx, int dy, int len){
+                for(var i =0; i < len; i++){
+                    if(map[X,Y])
+                        CrossDist.Add(Dist);
+                    map[X, Y] = true;
+                    X += dx;
+                    Y += dy;
+                }
+            }
+            private void Rotate(char rot){
+                if(rot == 'R')
+                    switch (dir){
+                        case 'N': dir = 'E'; break;
+                        case 'E': dir = 'S'; break;
+                        case 'S': dir = 'W'; break;
+                        case 'W': dir = 'N'; break;
+                    }
+                else
+                    switch (dir){
+                        case 'N': dir = 'W'; break;
+                        case 'E': dir = 'N'; break;
+                        case 'S': dir = 'E'; break;
+                        case 'W': dir = 'S'; break;
+                    }
+            }
+        }
     }
 }
 namespace AoC2016.Days.Day1{
--- AoC2016/DayBase.cs
+++ AoC2016/DayBase.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Linq;
 namespace AoC2016
 {
-    public abstract class DayBase
+    public abstract partial class DayBase
     {
         private readonly int dayNum;
         protected IList<Input> tests = new List<Input>();
@@ -12,65 +12,64 @@ namespace AoC2016
         {
             this.dayNum = dayNum;
         }
-
-        public DayBase Input(string name)
-        {
-            var a = new Input($"./Inputs/Day{dayNum}/{name}.txt", name);
-            tests.Add(a);
-            return this;
-        }
-        public DayBase RunPart(byte part) => RunPart<string>(part, null, false);
-        public DayBase RunPart<T>(byte part, T result = default(T), bool rerun = false){
-            if(part < 1 || part > 2)
-                throw new InvalidDataException("Parts to run should be 1 or 2");
-            tests.Last().Tests[part-1] = true;
-
-            if(!EqualityComparer<T>.Default.Equals(result, default(T))){
-                tests.Last().Result[part-1] = Tuple.Create((object?)result, typeof(T));
-            }
-
-            return this;
-        }
         public abstract object Part1(Input input);
         public abstract object Part2(Input input);
 
-        public virtual void Execute(){
-            foreach(var test in tests.Where(t => t.Tests.All(b => b == null))){
-                test.Tests[0] = true;
-                test.Tests[1] = true;
-            }
-            
+        public virtual void Execute(){           
             ProcessTest(0, Part1);
             ProcessTest(1, Part2);
         }
 
         private void ProcessTest(byte testNum, Func<Input, object> testFunc){
-            foreach(var test in tests.Where(t => t.Tests[testNum].HasValue && t.Tests[testNum].Value)){
-                Console.WriteLine($"{test.Name} part {testNum+1}:");
-
+            Console.WriteLine($"==== Part {testNum + 1} ====");
+            foreach(var test in tests.Where(t => t.Tests[testNum] != AoC2016.Input.TestType.Skip)){
                 var resultObj = testFunc(test);
+
+                if(test.Tests[testNum] == AoC2016.Input.TestType.Silent)
+                    continue;
+                
+                Console.WriteLine($"\t{test.Name}");
                 var desiredResult = test.Result[testNum];
                 
                 if(resultObj != null){
-                    if(desiredResult.Item1 == null){
+                    if(desiredResult?.Item1 == null){
                         Console.WriteLine($"Result[ {resultObj} ]");
                     }
                     else if(resultObj.GetType() != desiredResult.Item2){
                         Console.WriteLine($"Result[ {resultObj} ] Expected[ {desiredResult.Item1} ]");
                     }else{
-                        var comparer = typeof(EqualityComparer<>)
-                            .MakeGenericType(desiredResult.Item2)
-                            .GetProperty("Default", System.Reflection.BindingFlags.Default | System.Reflection.BindingFlags.Static)
-                            .GetValue(null);
-                            //.Equals(resultObj, desiredResult);
-                        if(comparer.Equals(resultObj, desiredResult.Item1)){
-                            Console.WriteLine$($"<< {resultObj} >>");
-                        }else{
-                            Console.WriteLine("dupa");
+                        if (resultObj.GetType().GetInterface("IComparable")!= null){
+                            VerifyComparable((IComparable) resultObj, desiredResult.Item1);
                         }
                     }
                 }
-                Console.WriteLine($"{resultObj} {resultObj.GetType()}");
+            }
+        }
+
+        private void VerifyComparable(IComparable compResult, object desired)
+        {
+            switch (compResult.CompareTo(desired))
+            {
+                case -1:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[ {compResult} ] too low Expected[ { desired} ]");
+                    Console.ResetColor();
+                    break;
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[ {compResult} ] too High Expected[ { desired} ]");
+                    Console.ResetColor();
+                    break;
+                case 0:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Correct[ {compResult} ]");
+                    Console.ResetColor();
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Invalid compare Result[ {compResult} ] <> Expected[ { desired} ]");
+                    Console.ResetColor();
+                    break;
             }
         }
     }
@@ -88,32 +87,5 @@ namespace AoC2016
         public override object Part1(Input input) => Part1(input.Prepare<T>(Split, Parse), input);
 
         public override object Part2(Input input) => Part2(input.Prepare<T>(Split, Parse), input);
-    }
-
-    public class Input{
-        public Input(string Path, string name){
-            _filePath = Path;
-            Name = name;
-        }
-        public string Raw{
-            get{ return File.ReadAllText(_filePath);}
-        }
-        private readonly string _filePath;
-        public readonly bool?[] Tests = new bool?[2];
-        public readonly Tuple<object, Type>[] Result = new Tuple<object, Type>[2];
-        public readonly string Name;
-        private IList<object> _storedInput;
-
-        public IList<string> Split(Func<string, IList<string>> splitFunc){
-            return splitFunc(Raw.Trim()).Select(s => s.Trim()).ToList();
-        }
-        public IList<T> Prepare<T>(Func<string, IList<string>> splitFunc, Func<string, T> transformFunc, bool force=false){
-            if (_storedInput == null || force == true){
-                _storedInput = new List<object>();
-                foreach(var line in Split(splitFunc))
-                    _storedInput.Add(transformFunc(line));
-            }
-            return _storedInput.Select(o => (T)o).ToList();
-        }
     }
 }
