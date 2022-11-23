d83b2053a3ec0536d780ced5ae4f1161fb0a9264
Michał Wojtowicz <michal.wojtowicz@aspiresys.com>
2022-11-23 14:53:52 +0100

    AoC2016 Day 6 both tasks

--- AoC2016/Day6.cs
+++ AoC2016/Day6.cs
@@ -13,20 +13,46 @@ namespace AoC2016
         public Day6() : base(6)
         {
             Input("example1")
-                .RunPart(1, 5)
-            .Input("output");
+                .RunPart(1, "easter")
+                .RunPart(2, "advent")
+            .Input("output")
+                .RunPart(1, "tzstqsua")
+                .RunPart(2, "myregdnr");
         }
 
         public override object Part1(Input input)
         {
-            throw new NotImplementedException();
+            var stats = new Dictionary<char, int>[input.Lines[0].Length];
+            for(var i=0; i<stats.Length; i++)
+                stats[i] = new Dictionary<char, int>();
+
+            foreach(var line in input.Lines){
+                for(var i=0; i<line.Length; i++){
+                    if(stats[i].ContainsKey(line[i]))
+                        stats[i][line[i]]++;
+                    else
+                        stats[i][line[i]] = 1;
+                }
+            }
+
+            input.Cache = stats;
+
+            var result = string.Empty;
+            foreach(var dict in stats){
+                result += dict.OrderByDescending(d => d.Value).First().Key;
+            }
+            return result;
         }
 
         public override object Part2(Input input)
         {
-            throw new NotImplementedException();
+             var stats = (Dictionary<char, int>[])input.Cache;
+
+             var result = string.Empty;
+            foreach(var dict in stats){
+                result += dict.OrderBy(d => d.Value).First().Key;
+            }
+            return result;
         }
     }
-}
-namespace AoC2016.Days.Day2{
 }
