4cdf32c5ec2f81a5c82419f9844912538dacea05
Michał Wojtowicz <michal.wojtowicz@aspiresys.com>
2022-11-24 16:29:51 +0100

    aoc2016 Day 9 unsolved

--- AoC2016/Day9.cs
+++ AoC2016/Day9.cs
@@ -12,19 +12,66 @@ namespace AoC2016
     {
         public Day9() : base(9)
         {
-            Input("example1")
-                .RunPart(1, 5)
-            .Input("output");
+            Input("ex1")
+                .RunPart(1, 6);
+            Input("ex2")
+                .RunPart(1, 7);
+            Input("ex3")
+                .RunPart(1, 9);
+            Input("ex4")
+                .RunPart(1, 11);
+            Input("ex5")
+                .RunPart(1, 6);
+            Input("ex6")
+                .RunPart(1, 18);
+            Input("output")
+                .TooLow(1, 112585);
         }
 
         public override object Part1(Input input)
         {
-            throw new NotImplementedException();
+            var word  = input.Raw;
+            var output = string.Empty;
+
+            var i = 0;
+            while(i<word.Length){
+                if(word[i] == '('){
+                    var len = ReadNum(word, ref i);
+                    i++; // x
+                    var reps = ReadNum(word, ref i);
+                    i++; // )
+
+                    var source = string.Empty;
+                    for(;len > 0 && i<=word.Length; len--){
+                        
+                        source += word[i+1];
+                        i++;
+                    }
+                    for(; reps>0; reps--)
+                    output += source;
+                }else{
+                    output += word[i];
+                }
+                i++;
+            }
+
+            return output.Length;
         }
 
         public override object Part2(Input input)
         {
             throw new NotImplementedException();
+        }
+
+        private int ReadNum(string val, ref int ctr){
+            var result = 0;
+            ctr++;
+            while(val[ctr] >= '0' && val[ctr] < '9'){
+                result = result*10 + (val[ctr]-'0');
+                ctr++;
+            }
+            ctr--;
+            return result;
         }
     }
 }
