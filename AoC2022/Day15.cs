6c6cdad243486901e9bffeb32d2b098c5fd73078
misiakw <misiakw@gmail.com>
2022-12-15 07:52:34 +0100

    AoC'22 Day 15 part 1

--- AoC2022/Day15.cs
+++ AoC2022/Day15.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using AoC.Base;
+using AoC.Common;
 
 namespace AoC2022
 {
@@ -13,18 +14,87 @@ namespace AoC2022
         public Day15() : base(15)
         {
             Input("example1")
-                .RunPart(1, 5)
-            .Input("output");
+                .RunPart(1, 26)
+                .RunPart(2, 56000011)
+            .Input("output")
+                .RunPart(1, 6275922);
         }
 
         public override object Part1(Input input)
         {
-            throw new NotImplementedException();
+            var searchLine = input.Name == "example1" ? 10 : 2000000;
+            input.Cache = PrepareInput(input.Lines);
+            var sensors = (IList<Sensor>)((Tuple<List<Sensor>, List<Beacon>>)input.Cache).Item1;
+            var beacons = (IList<Beacon>)((Tuple<List<Sensor>, List<Beacon>>)input.Cache).Item2;
+
+            var considerable = sensors.Where(s => Math.Abs(searchLine - s.Y) <= s.Range).ToList();
+
+            var line = new Dictionary<long, bool>();
+            foreach(var sensor in considerable){
+                for(long x = sensor.X; sensor.IsInRange(x, searchLine); x++)
+                    if(!line.ContainsKey(x))
+                        line.Add(x, true);
+                
+                for(long x = sensor.X; sensor.IsInRange(x, searchLine); x--)
+                    if(!line.ContainsKey(x))
+                        line.Add(x, true);
+            }
+
+            return line.Count - beacons.Where(b => b.Y == searchLine && line.Keys.Contains(b.X)).Count();
         }
 
         public override object Part2(Input input)
         {
-            throw new NotImplementedException();
+            var maxVal = input.Name == "example1" ? 20 : 4000000;
+            var sensors = (IList<Sensor>)((Tuple<List<Sensor>, List<Beacon>>)input.Cache).Item1;
+            var beacons = (IList<Beacon>)((Tuple<List<Sensor>, List<Beacon>>)input.Cache).Item2;
+
+            var map = new Array2D<byte>(0);
+
+            foreach(var sensor in sensors.Take(3))
+                map = sensor.FillMap(map, maxVal);
+
+            Console.WriteLine(map.Draw(b => b == 0 ? ".": $"{(char)b}"));
+
+            return maxVal;
+        }
+
+        private Tuple<List<Sensor>, List<Beacon>> PrepareInput(IList<string> lines){
+            var sensors = new List<Sensor>();
+            var beacons = new Dictionary<string, Beacon>();
+
+            foreach(var line in lines){
+                var parts = line.Replace("Sensor at ", "")
+                    .Replace(" closest beacon is at ", "")
+                    .Replace("x=", "").Replace(" y=", "").Split(":")
+                    .Select(s => s.Split(",").Select(long.Parse).ToArray())
+                    .ToArray();
+
+
+                if(!beacons.ContainsKey($"{parts[1][0]}|{parts[1][1]}"))
+                    beacons.Add($"{parts[1][0]}|{parts[1][1]}", new Beacon(parts[1][0], parts[1][1]));
+                sensors.Add(new Sensor(parts[0][0], parts[0][1], beacons[$"{parts[1][0]}|{parts[1][1]}"]));
+            }
+
+            return Tuple.Create(sensors, beacons.Values.ToList());
+        }
+
+        protected class Sensor : Point
+        {
+            private Beacon beacon;
+            public readonly long Range;
+            public Sensor(long X, long Y, Beacon beacon) : base(X, Y)
+            {
+                this.beacon = beacon;
+                this.Range = Math.Abs(beacon.X - X) + Math.Abs(beacon.Y - Y);
+            }
+            public bool IsInRange(long x, long y) => Math.Abs(x - X) + Math.Abs(y - Y) <= Range;
+        }
+        protected class Beacon : Point
+        {
+            public Beacon(long X, long Y) : base(X, Y)
+            {
+            }
         }
     }
 }
--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -6,7 +6,7 @@ namespace AoC2022
     {
         static void Main(string[] args)
         {
-            new Launcher().Run(14);
+            new Launcher().Run(15);
             /*var day = new Day7();
             day.Execute();*/
         }
