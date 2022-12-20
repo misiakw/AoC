3b9f7382ab18fbabaa69f5e1cda7d596a1b3e5da
misiakw <misiakw@gmail.com>
2022-12-20 22:05:24 +0100

    AoC'22 day 20 both

--- AoC2022/Day20.cs
+++ AoC2022/Day20.cs
@@ -13,82 +13,97 @@ namespace AoC2022
         public Day20() : base(20)
         {
             Input("example1")
-                .RunPart(1, 3)
-            .Input("output");
+                .RunPart(1, 3L)
+                .RunPart(2, 1623178306L)
+            .Input("myExamle")
+                //.RunPart(1)
+            .Input("output")
+                .RunPart(1, 2203L)
+                .RunPart(2, 6641234038999L);
         }
 
         public override object Part1(Input input)
         {
-            var orig = input.Lines.Select(int.Parse).ToArray();
-
-            var circle = new Day20Circle(orig);
-
-            Console.WriteLine(string.Join(", ", circle.ToList()));
+            var circle = new Day20Circle(input.Lines.Select(long.Parse).ToArray());
             circle.Mix();
-            Console.WriteLine(string.Join(", ", circle.ToList()));
+            circle.Zero();
 
-            return 0;
+            var a = circle[1000];
+            var b=  circle[2000];
+            var c = circle[3000];
+
+            return a+b+c;
         }
 
         public override object Part2(Input input)
         {
-            throw new NotImplementedException();
+            var orig = input.Lines.Select(long.Parse).ToArray();
+            for(var i=0; i< orig.Length; i++)
+                orig[i] *= 811589153;
+
+            var circle = new Day20Circle(orig);
+            for(int i=0; i<10; i++)
+                circle.Mix();
+            circle.Zero();
+
+            var a = circle[1000];
+            var b=  circle[2000];
+            var c = circle[3000];
+
+            return a+b+c;
         }
     }
 
     public class Circle<T>{
-        protected int currentPos = 0;
         public int Len {get; protected set; } = 0;
-        protected CircleObj<T>? Elem;
+        protected CircleObj<T> Start;
         
         public Circle(T[] data){
-            Elem = new CircleObj<T>(data[0]);
+            Start = new CircleObj<T>(data[0]);
             Len = data.Length;
-            var tmp = Elem;
+            var tmp = Start;
             for(var i=1; i<Len; i++){
                 tmp.Right = new CircleObj<T>(data[i]);
                 tmp.Right.Left = tmp;
                 tmp = tmp.Right;
             }
-            Elem.Left = tmp;
-            tmp.Right = Elem;
+            Start.Left = tmp;
+            tmp.Right = Start;
         }
 
         public IList<T> ToList(){
             var result = new List<T>();
-            for(var x=0; x<Len; x++)
-                result.Add(this[x]);
+            var el = Start;
+            for(var x=0; x<Len; x++){
+                result.Add(el.Value);
+                el = el.Right;
+            }
             return result;
         }
 
-        public T this[int x]{
-            get {
-                GoTo(x);
-                return Elem.Value;
-            }
+        public T this[long x]{
+            get => Get(x).Value;
         }
 
-        protected CircleObj<T> GoTo(int x){
+        protected CircleObj<T> Get(long x){
             var pos = x % Len;
-            if (pos < 0) pos = Len + pos;
-
-            if (currentPos < pos)
-            {
-                while (currentPos < pos)
-                {
-                    Elem = Elem.Right;
-                    currentPos++;
+                if (pos < 0) pos = Len + pos;
+                var el = Start;
+                var hops = 0;
+                if(pos > Len/2){
+                    el = Start.Left;
+                    hops++;
+                    for(var i=Len-1; i>pos; i--){
+                        el = el.Left;
+                        hops ++;
+                    }
+                }else{
+                    for(var i=0; i<pos; i++){
+                        el = el.Right;
+                        hops ++;
+                    }
                 }
-            }
-            else if (currentPos > pos)
-            {
-                while (currentPos > pos)
-                {
-                    Elem = Elem.Left;
-                    currentPos--;
-                }
-            }
-            return Elem;
+                return el;
         }
 
         protected class CircleObj<V>{
@@ -102,21 +117,64 @@ namespace AoC2022
             public override string ToString() => Value.ToString();
         }
 
-        public override string ToString() => $"Circle({Elem})";
+        public override string ToString() => $"Circle({Start})";
     }
 
-    public class Day20Circle : Circle<int>
+    public class Day20Circle : Circle<long>
     {
-        private CircleObj<int>[] baseOrder;
-        public Day20Circle(int[] data) : base(data)
+        private CircleObj<long>[] baseOrder;
+        public Day20Circle(long[] data) : base(data)
         {
-            baseOrder = new CircleObj<int>[data.Length];
+            baseOrder = new CircleObj<long>[data.Length];
             for(var i=0; i<data.Length; i++)
-                baseOrder[i] = GoTo(i);
+                baseOrder[i] = Get(i);
         }
 
         public void Mix(){
+            foreach(var el in baseOrder){
+                if (el.Value == 0){
+                    continue;
+                }
+                Pop(el);
+                var next = el.Value > 0 
+                    ? Right(el, el.Value%(Len-1))
+                    : Left(el, (el.Value%(Len-1)*-1));
+                var right = el.Value > 0 ? next.Right : next;
+                var left = el.Value > 0 ? next : next.Left;
+                PutBetween(el, left, right);
+            }
+        }
 
+        private CircleObj<long> Right(CircleObj<long> el, long num){
+            while(num-->0)
+                el = el.Right;
+            return el;
+        }
+        private CircleObj<long> Left(CircleObj<long> el, long num){
+            while(num-->0)
+                el = el.Left;
+            return el;
+        }
+
+        private void Pop(CircleObj<long> obj){
+            obj.Left.Right = obj.Right;
+            obj.Right.Left = obj.Left;
+            if (obj == Start)
+                Start = obj.Right;
+        }
+
+        private void PutBetween(CircleObj<long> obj, CircleObj<long> l, CircleObj<long> r){
+            obj.Left = l;
+            obj.Right = r;
+            l.Right = obj;
+            r.Left = obj;
+        }
+
+        public void Zero(){
+            var ns = Start;
+            while(ns.Value != 0)
+                ns = ns.Right;
+            Start = ns;
         }
     }
 
