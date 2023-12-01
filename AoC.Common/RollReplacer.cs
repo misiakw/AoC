fa6a4c4764637e259d7a8714437ee9f789b9d44a
Michał Wojtowicz <michal.wojtowicz@aspiresys.com>
2023-12-01 13:54:14 +0100

    AoC 2023 day 1 part 2 solved

---
+++ AoC.Common/RollReplacer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace AoC.Common
+{
+    public class RollReplacer
+    {
+        private RepNode _root = new RepNode();
+        public RollReplacer(IList<(string, string)> replacements)
+        {
+            foreach (var item in replacements)
+                this._root.Add(item.Item1, item.Item2);
+        }
+
+        public string Replace(string input)
+        {
+            var result = string.Empty;
+            var buff = new Queue<char>();
+
+            foreach (var ch in input)
+            {
+                buff.Enqueue(ch);
+                var currKey = new string(buff.ToArray());
+                if (this._root.CanReachValue(currKey))
+                {
+                    var val = this._root.GetValue(currKey);
+                    if (val != null)
+                    {
+                        result = result + val;
+                        buff.Clear();
+                    }
+                }
+                else
+                {
+                    while (buff.Count > 0 && !this._root.CanReachValue(currKey))
+                    {
+                        result += buff.Dequeue();
+                        currKey = new string(buff.ToArray());
+                        if (this._root.CanReachValue(currKey))
+                        {
+                            var val = this._root.GetValue(currKey);
+                            if (val != null)
+                            {
+                                result = result + val;
+                                buff.Clear();
+                            }
+                        }
+                    }
+                }
+            }
+            while (buff.Count > 0)
+                result += buff.Dequeue();
+
+            return result;
+        }
+
+        private class RepNode
+        {
+            public IDictionary<char, RepNode> Nodes = new Dictionary<char, RepNode>();
+            private string? _value = null;
+
+            public void Add(string key, string value)
+            {
+                if (string.IsNullOrEmpty(key))
+                    this._value = value;
+                else
+                {
+                    var newK = key[0];
+                    if (!Nodes.ContainsKey(newK))
+                        Nodes.Add(newK, new RepNode());
+                    Nodes[newK].Add(key.Substring(1), value);
+                }
+            }
+
+            public bool CanReachValue(string key)
+            {
+                if (string.IsNullOrEmpty(key))
+                    return true;
+                if (!this.Nodes.ContainsKey(key[0]))
+                    return false;
+                return Nodes[key[0]].CanReachValue(key.Substring(1));
+            }
+            public string? GetValue(string key)
+                => string.IsNullOrEmpty(key)
+                ? this._value
+                : this.Nodes[key[0]].GetValue(key.Substring(1));
+        }
+    }
+}
--- AoC2023/Days/Day1.cs
+++ AoC2023/Days/Day1.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AoC.Base;
 using AoC.Base.TestInputs;
+using AoC.Common;
 
 
 namespace AoC2023.Days
@@ -42,11 +40,30 @@ namespace AoC2023.Days
             var sum = 0;
             foreach (var line in lines)
             {
-                var line2 = replacer.Replace(line);
+                var left = -1;
+                var right = -1;
+                var len = 1;
+                var tmp = string.Empty;
+                var lineLen = line.Length;
 
-                var tmp = (line2.First(c => c >= '0' && c <= '9') - '0') * 10;
-                tmp += (line2.Last(c => c >= '0' && c <= '9') - '0');
-                sum += tmp;
+                while(left == -1 || right == -1)
+                {
+                    if (left == -1)
+                    {
+                        tmp = replacer.Replace(line.Substring(0, len));
+                        if (tmp.Any(c => c >= '0' && c <= '9'))
+                            left = (tmp.First(c => c >= '0' && c <= '9') - '0') * 10;
+                    }
+                    if(right == -1)
+                    {
+                        tmp = replacer.Replace(line.Substring(lineLen - len));
+                        if (tmp[0] >= '0' && tmp[0] <= '9')
+                            right = tmp[0] - '0';
+                    }
+                    len++;
+                }
+
+                sum += left + right;
             }
             return sum;
         }
@@ -59,90 +76,7 @@ namespace AoC2023.Days
                .Part2(281);
             builder.New("output", "./Inputs/Day1/output.txt")
                 .Part1(55834)
-                .Part2(0); //52606 too low, 53254 too high
-        }
-
-        private class RollReplacer
-        {
-            private RepNode _root = new RepNode();
-            public RollReplacer(IList<(string, string)> replacements)
-            {
-                foreach (var item in replacements)
-                    this._root.Add(item.Item1, item.Item2);
-            }
-
-            public string Replace(string input)
-            {
-                var result = string.Empty;
-
-                var buff = new Queue<char>();
-
-                foreach(var ch in input)
-                {
-                    buff.Enqueue(ch);
-                    var currKey = new string(buff.ToArray());
-                    if (this._root.CanReachValue(currKey))
-                    {
-                        var val = this._root.GetValue(currKey);
-                        if(val != null)
-                        {
-                            result = result + val;
-                            buff.Clear();
-                        }
-                    }
-                    else
-                    {
-                        while(buff.Count > 0 && !this._root.CanReachValue(currKey))
-                        {
-                            result += buff.Dequeue();
-                            currKey = new string(buff.ToArray());
-                            if (this._root.CanReachValue(currKey))
-                            {
-                                var val = this._root.GetValue(currKey);
-                                if (val != null)
-                                {
-                                    result = result + val;
-                                    buff.Clear();
-                                }
-                            }
-                        }
-                    }
-                }
-
-                return result;
-            }
-
-            private class RepNode
-            {
-                public IDictionary<char, RepNode> Nodes = new Dictionary<char, RepNode>();
-                private  string? _value = null;
-
-                public void Add(string key, string value)
-                {
-                    if (string.IsNullOrEmpty(key))
-                        this._value = value;
-                    else
-                    {
-                        var newK = key[0];
-                        if (!Nodes.ContainsKey(newK))
-                            Nodes.Add(newK, new RepNode());
-                        Nodes[newK].Add(key.Substring(1), value);
-                    }
-                }
-
-                public bool CanReachValue(string key)
-                {
-                    if (string.IsNullOrEmpty(key))
-                        return true;
-                    if (!this.Nodes.ContainsKey(key[0]))
-                        return false;
-                    return Nodes[key[0]].CanReachValue(key.Substring(1));
-                }
-                public string? GetValue(string key)
-                    => string.IsNullOrEmpty(key)
-                    ? this._value
-                    : this.Nodes[key[0]].GetValue(key.Substring(1));
-            }
+                .Part2(53221);
         }
     }
 }
