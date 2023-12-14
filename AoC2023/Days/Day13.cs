cd3fee9d73bd3ad37b4b1a2b65df1cbc12683a86
Michał Wojtowicz <michal.wojtowicz@aspiresys.com>
2023-12-14 16:01:23 +0100

    AoC2023 D13 not solved

--- AoC2023/Days/Day13.cs
+++ AoC2023/Days/Day13.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using AoC.Base;
 using AoC.Base.TestInputs;
@@ -15,24 +16,24 @@ namespace AoC2023.Days
         {
             builder.New("example1", "./Inputs/Day13/example1.txt")
                .Part1(405)
-               .Part2(-1);
+               .Part2(400);
             builder.New("output", "./Inputs/Day13/output.txt")
                 .Part1(27300);
             //.Part2(790194712336);
         }
         public override long Part1(IComparableInput<long> input)
         {
-            var arrays = GetCharArrays(input);
-            return arrays.Sum(arr => arr.GetScore(0));
+            var arrays = GetCharArrays(input, 0);
+            return arrays.Sum(arr => arr.GetScore());
         }
 
         public override long Part2(IComparableInput<long> input)
         {
-            var arrays = GetCharArrays(input);
-            return arrays.Sum(arr => arr.GetScore(1));
+            var arrays = GetCharArrays(input, 1);
+            return arrays.Sum(arr => arr.GetScore());
         }
 
-        private IEnumerable<CharArray> GetCharArrays(IComparableInput<long> input)
+        private IEnumerable<CharArray> GetCharArrays(IComparableInput<long> input, int smugesToFix)
         {
             var lines = ReadLines(input);
             var map = new List<string>();
@@ -40,22 +41,24 @@ namespace AoC2023.Days
             {
                 if (string.IsNullOrEmpty(line))
                 {
-                    yield return new CharArray(map);
+                    yield return new CharArray(map, smugesToFix);
                     map.Clear();
                 }
                 else
                     map.Add(line);
             }
-            yield return new CharArray(map);
+            yield return new CharArray(map, smugesToFix);
         }
 
         private class CharArray
         {
             public readonly int Width, Height;
             public char[,] Map;
+            private int smugesToFix = 0;
 
-            public CharArray(IList<string> lines)
+            public CharArray(IList<string> lines, int smugesToFix)
             {
+                this.smugesToFix = smugesToFix;
                 Width = lines[0].Length;
                 Height = lines.Count();
                 Map = new char[Width, Height];
@@ -64,17 +67,17 @@ namespace AoC2023.Days
                         Map[x, y] = lines[y][x];
             }
 
-            public int GetScore(int smugesToFix)
+            public int GetScore()
             {
-                var horizontal = SearchForMirrorRows(smugesToFix);// -
-                var vertical = SearchForMirrorCollumns(smugesToFix); // |
+                var horizontal = SearchForMirrorRows();// -
+                var vertical = SearchForMirrorCollumns(); // |
 
                 return (vertical > 0 ? vertical : 0) + 100 * (horizontal > 0 ? horizontal : 0);
             }
 
-            private int SearchForMirrorCollumns(int smugesToFix) => SearchForMirror(true, smugesToFix);
-            private int SearchForMirrorRows(int smugesToFix) => SearchForMirror(false,  smugesToFix);
-            private int SearchForMirror(bool isCollumn, int smugesToFix)
+            private int SearchForMirrorCollumns() => SearchForMirror(true);
+            private int SearchForMirrorRows() => SearchForMirror(false);
+            private int SearchForMirror(bool isCollumn)
             {
                 var eqLimit = isCollumn ? Height : Width;
                 var searchLimit = isCollumn ? Width : Height;
@@ -91,10 +94,19 @@ namespace AoC2023.Days
                     for (var j = 0; i - j >= 0 && i + j + 1 < searchLimit; j++)
                     {
                         var smuges = SmugeCount(i - j, i + j + 1, dataFunc, eqLimit);
-                        if (smuges <= smugesToFix)
+                        if (smuges <= smugesToFix && smuges > 0)
                         {
+                            Console.WriteLine(Draw());
                             FixSmuge(i - j, i + j + 1, dataFunc, eqLimit, fixFunc);
+                            Console.WriteLine($"Fix {(isCollumn ? "collumn" : "row")} {i + j + 1} into {i - j}");
+                            Console.WriteLine(Draw());
                             smugesToFix -= smuges;
+                            if(smugesToFix == 0)
+                            {
+                                i = -1;
+                                isMirrored = false;
+                                break;
+                            }
                         }
                         if (smuges > smugesToFix)
                         {
@@ -125,6 +137,18 @@ namespace AoC2023.Days
                     if (data(trgt, i) != data(src, i))
                         change(trgt, src, i);
                 }
+            }
+
+            public string Draw()
+            {
+                var sb = new StringBuilder();
+                for(var y=0; y<Height; y++)
+                {
+                    for (var x = 0; x < Width; x++)
+                        sb.Append(Map[x, y]);
+                    sb.AppendLine();
+                }
+                return sb.ToString();
             }
         }
     }
