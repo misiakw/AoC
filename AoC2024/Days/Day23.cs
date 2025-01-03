9b79dac659e2b5a0d0fbb13c0d8dd40adf04bba9
Michał Wojtowicz <michal.wojtowicz@aspiresys.com>
2025-01-03 12:25:14 +0100

    AoC'24 d23 p2

--- AoC2024/Days/Day23.cs
+++ AoC2024/Days/Day23.cs
@@ -12,11 +12,11 @@ namespace AoC2024.Days
     {
         public static void RunAoC() => AocRuntime.Day<Day23>(23, t => new Day23(t.GetLines()))
                 .Callback(1, (d, t) => d.Part1())
-                //.Callback(2, (d, t) => d.Part2())
+                .Callback(2, (d, t) => d.Part2())
                 .Test("example")
                 .Test("input")
                 //.Part(1).Correct(1149)
-                //.Part(2).Correct()
+                //.Part(2).Correct("as,co,do,kh,km,mc,np,nt,un,uq,wc,wz,yo")
                 .Run();
 
         public Day23(IEnumerable<string> lines)
@@ -37,7 +37,7 @@ namespace AoC2024.Days
         {
             var dict = new Dictionary<string, string[]>();
 
-            foreach (var firstKey in Nodes.Keys.Where(k => k.StartsWith("t")))
+            foreach (var firstKey in Nodes.Keys)
             {
                 var first = Nodes[firstKey];
                 foreach (var second in first.Neighbours)
@@ -47,18 +47,46 @@ namespace AoC2024.Days
                             var names = new string[] { first.Name, second.Name, thirth.Name };
                             var key = string.Join(",", names.Order());
                             if (!dict.ContainsKey(key))
+                            {
                                 dict.Add(key, names);
+                            }
                         }
             }
-
-            return dict.Count().ToString();
+            return dict.Where(d => d.Value.Any(t => t.StartsWith("t"))).Count().ToString();
         }
 
+        public string Part2()
+        {
+            if (Nodes.Count == 0)
+                Part1();
+
+            var cliques = new List<Clique>();
+            foreach (var start in Nodes.Values.OrderBy(n => n.Neighbours.Count()))
+            {
+                var clique = new Clique();
+                clique.Nodes.Add(start);
+
+                foreach (var node in Nodes.Values)
+                {
+                    if (node == start) continue;
+                    if (clique.Nodes.All(node.Neighbours.Contains))
+                        clique.Nodes.Add(node);
+                }
+                cliques.Add(clique);
+            }
+
+
+            return cliques.OrderByDescending(c => c.Nodes.Count()).First().ToString();
+        }
+
+        private IDictionary<int, int> NodeCount = new Dictionary<int, int>();
         private IDictionary<string, Node> Nodes = new Dictionary<string, Node>();
         private Node GetNode(string name)
         {
             if (!Nodes.ContainsKey(name))
+            {
                 Nodes.Add(name, new Node(name));
+            }
             return Nodes[name];
         }
 
@@ -70,10 +98,17 @@ namespace AoC2024.Days
                 if (!Neighbours.Contains(node))
                     Neighbours.Add(node);
             }
-            
+            public IList<Clique> Cliques = new List<Clique>();
             public Node(string name) { Name = name; }
             public readonly string Name;
             public IList<Node> Neighbours = new List<Node>();
+            public override string ToString() => Name;
+        }
+
+        private class Clique
+        {
+            public IList<Node> Nodes = new List<Node>();
+            public override string ToString() => string.Join(",", Nodes.Select(n => n.Name).Order());
         }
     }
 }
